6e91f92e331c2083e92d677c1d41a11c68f1ebcb
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-05-16 14:41:36 +0500

    cd

--- live coding/app.js
+++ live coding/app.js
@@ -1,5 +1,9 @@
-// for (let i = 0; i < i < 5; i++) {
-//   setTimeout((i) => {
-//     console.log(i);
-//   }, 0);
-// }
+for (let i = 0; i < i < 5; i++) {
+  setTimeout(
+    (i) => {
+      console.log(i);
+    },
+    0,
+    i
+  );
+}
