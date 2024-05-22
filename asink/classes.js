da476fa0c4e9bdc5ea9b2bdd32dccf52a3a08def
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-05-22 10:28:40 +0500

    first commit

---
+++ asink/classes.js
@@ -0,0 +1,24 @@
+class Hero {
+  constructor() {}
+  sayHello() {
+    console.log("hello");
+  }
+}
+class Hero extends Gero {
+  constructor() {}
+  sayHello() {
+    console.log("hello");
+  }
+}
+class Hero extends Fero {
+  constructor() {}
+  sayHello() {
+    console.log("hello");
+  }
+}
+class Hero extends Cinema {
+  constructor() {}
+  sayHello() {
+    console.log("hello");
+  }
+}
--- class/app.js
+++ class/app.js
@@ -1,16 +1,18 @@
 class Chess {
-  constructor(name, walking, position, step) {
+  constructor(name, walking, position, step = 1) {
     this.name = name;
     this.walking = walking;
     this.position = position;
     this.step = step;
   }
-  move() {}
+  move() {
+    this.step++;
+  }
   lose() {}
 }
 
-const White_A2 = new Chess("Pawn", [1, 2], "A2", 1);
-const White_B2 = new Chess("Pawn", [1, 2], "B2", 2);
+const White_A2 = new Chess("Pawn", [1, 2], "A2");
+const White_B2 = new Chess("Pawn", [1, 2], "B2");
 const White_C2 = new Chess("Pawn", [1, 2], "C2");
 const White_D2 = new Chess("Pawn", [1, 2], "D2");
 const White_E2 = new Chess("Pawn", [1, 2], "E2");
@@ -27,31 +29,61 @@ const White_F1 = new Chess("Bishop", [1], "F1");
 const White_G1 = new Chess("Knight", [1], "G1");
 const White_H1 = new Chess("Rook", [1], "H1");
 //-------------------------------------------------
-const Black_A2 = new Chess("Pawn", [1, 2], "A8");
-const Black_B2 = new Chess("Pawn", [1, 2], "B8");
-const Black_C2 = new Chess("Pawn", [1, 2], "C8");
-const Black_D2 = new Chess("Pawn", [1, 2], "D8");
-const Black_E2 = new Chess("Pawn", [1, 2], "E8");
-const Black_F2 = new Chess("Pawn", [1, 2], "F8");
-const Black_G2 = new Chess("Pawn", [1, 2], "G8");
-const Black_H2 = new Chess("Pawn", [1, 2], "H8");
+const Black_A7 = new Chess("Pawn", [1, 2], "A8");
+const Black_B7 = new Chess("Pawn", [1, 2], "B8");
+const Black_C7 = new Chess("Pawn", [1, 2], "C8");
+const Black_D7 = new Chess("Pawn", [1, 2], "D8");
+const Black_E7 = new Chess("Pawn", [1, 2], "E8");
+const Black_F7 = new Chess("Pawn", [1, 2], "F8");
+const Black_G7 = new Chess("Pawn", [1, 2], "G8");
+const Black_H7 = new Chess("Pawn", [1, 2], "H8");
 //-----------------------------------------------------
-const Black_A1 = new Chess("Rook", [1], "A7");
-const Black_B1 = new Chess("Knight", [1], "B7");
-const Black_C1 = new Chess("Bishop", [1], "C7");
-const Black_D1 = new Chess("King", [1], "D7");
-const Black_E1 = new Chess("Queen", [1], "E7");
-const Black_F1 = new Chess("Bishop", [1], "F7");
-const Black_G1 = new Chess("Knight", [1], "G7");
-const Black_H1 = new Chess("Rook", [1], "H7");
+const Black_A8 = new Chess("Rook", [1], "A7");
+const Black_B8 = new Chess("Knight", [1], "B7");
+const Black_C8 = new Chess("Bishop", [1], "C7");
+const Black_D8 = new Chess("King", [1], "D7");
+const Black_E8 = new Chess("Queen", [1], "E7");
+const Black_F8 = new Chess("Bishop", [1], "F7");
+const Black_G8 = new Chess("Knight", [1], "G7");
+const Black_H8 = new Chess("Rook", [1], "H7");
 
-var obj = { White_A2 };
-function pawnA2Run(e, name) {
-  e.style.transform = `translateY(-${obj[name].step * 90}px)`;
-  obj[name] = { ...obj[name], step: obj[name].step + 1 };
+var obj = {
+  White_A2,
+  White_B2,
+  White_C2,
+  White_D2,
+  White_E2,
+  White_F2,
+  White_G2,
+  White_H2,
+  White_D1,
+};
+
+function pawnMove(e, name) {
+  if (obj[name]) {
+    e.style.transform = `translateY(-${obj[name].step * 90}px)`;
+    obj[name].move();
+  } else {
+    console.error(`Chess piece ${name} not found.`);
+  }
 }
-// var obj = { White_B2 };
-// function pawnB2Run(e, name) {
-//   e.style.transform = `translateY(-${obj[name].step * 90}px)`;
-//   obj[name] = { ...obj[name], step: obj[name].step + 1 };
-// }
+
+const objBlack = {
+  Black_A7,
+  Black_B7,
+  Black_C7,
+  Black_D7,
+  Black_E7,
+  Black_F7,
+  Black_G7,
+  Black_H7,
+};
+
+function pawnMoveBlack(e, name) {
+  if (objBlack[name]) {
+    e.style.transform = `translateY(${objBlack[name].step * 90}px)`;
+    objBlack[name].move();
+  } else {
+    console.error(`Chess piece ${name} not found.`);
+  }
+}
