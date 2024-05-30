43ca4bd9f41888107da5476f70372fd77fa34d80
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-05-30 08:04:10 +0500

    add todo list

---
+++ todo-list/app.js
@@ -0,0 +1,42 @@
+const todoWrapper = document.getElementById("todo__wrapper");
+const input__text = document.getElementById("input__text");
+const todo__list = document.getElementById("todo__list");
+const arr = [];
+
+function listAdd(e) {
+  e.preventDefault();
+  if (e.target.title.value.trim() == "") {
+    return;
+  } else {
+    arr.push({
+      text: e.target.title.value,
+      id: Math.floor(Math.random() * 10000),
+    });
+    render();
+    e.target.title.value = "";
+  }
+}
+
+function render() {
+  todo__list.innerHTML = "";
+  for (let i = 0; i < arr.length; i++) {
+    todo__list.innerHTML += `
+      <div class="title__wrapper" data-id="${arr[i].id}">
+        ${arr[i].text} 
+        <div class="button__wrapper">
+          <button><i class="fa-solid fa-pen"></i> edit</button>
+          <button onclick="itemDelete(${arr[i].id})">delete</button>
+        </div>
+      </div>
+    `;
+  }
+}
+
+function itemDelete(id) {
+  const index = arr.findIndex((item) => item.id === id);
+  if (index !== -1) {
+    arr.splice(index, 1);
+    render();
+  }
+}
+function itemEdit(id) {}
