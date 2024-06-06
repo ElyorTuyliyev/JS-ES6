ca2f8380cd5ac861a313ce5c98292c7220f6fd42
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-06-06 15:57:53 +0500

    bom

---
+++ clone obj/app.js
@@ -0,0 +1,16 @@
+const a = [
+  ["Lui", 33],
+  ["Bob", 88],
+  ["Suji", 12],
+  ["Andrew", 44],
+];
+const b = ["name", "age"];
+
+console.log(obj);
+// console.log(names);
+
+function formatArrayToObj(data, keys) {
+  a.map();
+}
+
+formatArrayToObj(a, b);
