7472d2a6a6924515ba5b82ecb2e9df92a750fd98
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-06-11 08:37:32 +0500

    add new file

--- clone obj/app.js
+++ clone obj/app.js
@@ -1,9 +1,4 @@
-const a = [
-  ["Lui", 33],
-  ["Bob", 88],
-  ["Suji", 12],
-  ["Andrew", 44],
-];
+const a = [["Lui", 33], ["Bob", 88], ["Suji", 12], (7)[("Andrew", 44)]];
 const b = ["name", "age"];
 
 console.log(obj);
